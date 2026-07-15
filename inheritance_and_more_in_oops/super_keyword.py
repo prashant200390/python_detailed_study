e331c89e45d8160b251b6e2b696cc71a86341ce0
prashant200390 <prajwalofficial90@gmail.com>
2026-07-15 18:13:17 +0545

    added super() keyword use

---
+++ inheritance_and_more_in_oops/super_keyword.py
@@ -0,0 +1,16 @@
+class delhi:
+    def __init__(self):
+        print("This is from Delhi.")
+
+class mumbai(delhi):
+    def __init__(self):
+        super().__init__()
+        print('This is from Mumbai.')
+
+class kolkata(delhi):
+    def __init__(self):
+        super().__init__()
+        print("This is from Kolkata.")
+
+place = kolkata()
+# fyi super() keyword helps to execute function of parent class .
