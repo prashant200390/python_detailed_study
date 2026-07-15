54cb31e0baf7fda4614cb7996f08537536fb2597
prashant200390 <prajwalofficial90@gmail.com>
2026-07-15 22:16:41 +0545

    material updated

--- inheritance_and_more_in_oops/04_super_keyword.py
+++ inheritance_and_more_in_oops/04_super_keyword.py
@@ -1,16 +1,28 @@
-class delhi:
+class delhi ():
     def __init__(self):
-        print("This is from Delhi.")
+        print("this is from delhi !")
+    
+    @staticmethod
+    def population():
+        print("population is huge by the way !")
 
 class mumbai(delhi):
     def __init__(self):
+        print("this is from mumbai !")
         super().__init__()
-        print('This is from Mumbai.')
-
-class kolkata(delhi):
+        super().population()
+        
+class rajistan(mumbai):
     def __init__(self):
+        print("this is from rajistan !")
         super().__init__()
-        print("This is from Kolkata.")
 
-place = kolkata()
+location2 = rajistan()
 # fyi super() keyword helps to execute function of parent class.
+''' 
+output :
+
+this is from rajistan !
+this is from mumbai !
+this is from delhi !
+population is huge by the way ! '''
